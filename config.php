7c5907ccdd5e2f05fc7dd07d18937878532e3b75
ColdDark29 <53429191+ColdDark29@users.noreply.github.com>
2019-09-28 08:17:28 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+#Nama Anda
+$Username = "xxxxxxxx";
+
+#Wajib Di isi
+$X_User_Id      = "Facebook_xxxxxxxxxxxxxxx";
+$X_User_Token   = "xxxxxxxxxxxxx";
+$X_Invite_Code  = "xxxxxxx";
+$Authorization  = "Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$X_Google_Ad_Id = "xxxxx-xxxx-xxxc-xxxx-xxxxxxxx";
+?>
